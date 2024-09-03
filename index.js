20b4084d3b3caea52d1e2e17e41448de2a2d1b5c
rafaelzinnho <117875271+rafaelzinnho@users.noreply.github.com>
2024-09-03 22:56:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { log } = require("console");
+
+
+const primeiroNome = "Rafael";
+const sobrenome = "Onofre";
+const idade = 17;
+const cidadeNatal = "Curitiba";
+const altura = 1.78;
+
+
+console.log("O meu primeiro nome é",primeiroNome);
+console.log("O meu sobrenome é",sobrenome);
+console.log("A minha idade é",idade);
+console.log("A minha cidade natal é",cidadeNatal);
+console,log("A minha altura é",altura);
